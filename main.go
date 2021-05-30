5feb5faed9c275e1459e3155759ab631fc6afa5d
tani <tani@1-10.com>
2021-05-30 10:32:28 +0900

    Update

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// migrate schema
+	db.AutoMigrate(&Product{})
+
+	// create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// read
+	var product Product
+	db.First(&product, 1) // find product with primary key
+	db.First(&product, "code = ?", "D42") // find product with code 
+
+	// update
+	db.Model(&product).Update("Price", 200)
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	db.Delete(&product, 1)
+}
